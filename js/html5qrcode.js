fee6c7521014e7d3de7d39a6148dc66e2f979004
wedo-hk <hkzioncafe@gmail.com>
2026-08-31 23:06:37 +0800

    Delete js directory

--- js/html5qrcode.js
+++
@@ -1,9 +0,0 @@
-  function onScanSuccess(decodedText, decodedResult) {
-    html5QrcodeScanner.clear();
-    // gasAttend(decodedText);
-    document.getElementById('scan_res').innerHTML=decodedText;
-  }
-  let html5QrcodeScanner = new Html5QrcodeScanner(
-    "reader",
-    { fps: 10, qrbox: {width: 250, height: 250} },false);
-  html5QrcodeScanner.render(onScanSuccess);
