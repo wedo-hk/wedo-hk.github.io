669b8cfb19791743f0d9a81b0049f18cc0976883
wedo-hk <hkzioncafe@gmail.com>
2026-09-02 00:18:00 +0800

    Update const.js

--- js/const.js
+++ js/const.js
@@ -1,4 +1,4 @@
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbzFrj2UuD4VZVbR_rGVtcCYI7QkFD2-6gxB6wDeS031hYq6SOvqS-dFnr-oGZ_t2Kpb/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwy_XsGo02p3Ykrbmfj_xY4Lbv8iuQTEtaKtpPSVgcNn3URntLJ0urob0uUggE_5OHT/exec';
 const YOUR_CLIENT_ID = '416770209134-tjtguj3gstg24fvvrtrj890p533bsuim.apps.googleusercontent.com';
 const YOUR_REDIRECT_URI = 'https://wedo-hk.github.io';
 const alertModal = new bootstrap.Modal(document.getElementById('alertModal'), {backdrop: 'static', keyboard: false});
